c8e1a24d927067936aabfaae1801486240d73418
1 qwerty <121islam>
2022-09-12 06:05:58 -0700

    Initial Commit

---
+++ lesson 2/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  lesson 2
+//
+//  Created by kayudo on 12/9/22.
+//
+
+import Foundation
+
+func printString(string: String){
+    print(string)
+}
+printString(string: "abc")
+printString(string: "xxx")
+
+
+func solveSquare(num: Int){
+    print("Квадрат \(num) равен \(num * num)")
+}
+
+solveSquare(num: 6)
+
+
